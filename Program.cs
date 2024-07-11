dfd8025800bca0634f60c056b95b28cc21dd13c8
sufyan-rizvi <sofylaiq@gmail.com>
2024-07-11 21:03:57 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Fibonacci();
+        }
+
+        static void Fibonacci()
+        {
+            int myNumber;
+            int firstNum = 0, secondNum = 1, thirdNum;
+
+
+            Console.WriteLine("Enter the length of fibonacci number: ");
+            myNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The series is: ");
+            for (int i = 0; i < myNumber + 1; i++)
+            {
+                Console.WriteLine(firstNum);
+                thirdNum = firstNum + secondNum;
+                firstNum = secondNum;
+                secondNum = thirdNum;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
